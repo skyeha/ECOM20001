6c97cb7e7b75fc416f56c9afcab05167b1802a11
skuha <nhatanhhado@gmail.com>
2022-08-27 16:36:55 +1000

    new changes

--- Assignment 1/as1_rscript.R
+++ Assignment 1/as1_rscript.R
@@ -1,7 +1,9 @@
 # Load the dataset into the environment and neccessary libraries
 library('stargazer')
 library('ggplot2')
+library('dplyr')
 
+# Question 1
 # Dataset summary: contains annaual information on beer sales, beer taxes, and cigarret taxes from 1981 - 2007 in US (47 states)
 beer_data = read.csv("D:/Semester 2 2022/ECOM20001/Assignment 1/as1_beer.csv")
 
@@ -10,9 +12,48 @@ tax_st = stargazer(beer_data,
                    summary.stat = c('n', 'mean', 'sd', 'median', 'min', 'max'),
                    type = 'text', title = 'Descriptive Statistic for Beer Dataset',
                    out = 'beer_sumstat.txt')
-# Compute standard deviation for each field
+
+# Question 2
+# Compute sample means for each field
+beercons_mean = mean(beer_data$beercons)
+beertax_mean = mean(beer_data$beertax)
+cigtax_mean = mean(beer_data$cigtax)
+
+# Compute sample standard deviation for each field
 beercons_sd = sd(beer_data$beercons)
 beertax_sd = sd(beer_data$beertax)
 cigtax_sd = sd(beer_data$cigtax)
 
-#Change from Rstudio
+# Compute 95% Confidence Interval for each field 
+
+# Beercons
+beercons_95_low = beercons_mean - 1.96 * beercons_sd
+beercons_95_high = beercons_mean + 1.96 * beercons_sd
+
+# Beertax
+beertax_95_low = beertax_mean - 1.96 * beertax_sd
+beertax_95_high = beertax_mean + 1.96 * beertax_sd
+
+# Cigtax
+cigtax_95_low = cigtax_mean - 1.96 * cigtax_sd
+cigtax_95_high = cigtax_mean + 1.96 * cigtax_sd
+
+
+# Question 3
+
+beer_data2 = beer_data %>%
+  mutate(hightax = if_else(beertax >= median(beertax),
+                           1,
+                           0))
+beer_plot = ggplot(beer_data2, aes(x = beercons, fill = as.character(hightax))) +
+  geom_density(alpha = 0.25)
+
+beer_plot + labs(title = "Density Plot for beer sales",
+                 x = "Beercons (gallons per capita)",
+                 y = "Density",
+                 fill = "Hightax value") +
+            theme_bw() +
+            theme(plot.title = element_text(face = "bold", size = 16, hjust = 0.5),
+                  axis.title = element_text(face = "italic", size = 10))
+
+
