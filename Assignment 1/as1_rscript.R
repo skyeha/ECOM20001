491bcad06c5c4c98793dca28aef1713940a1eea6
skyeha <nhatanhhado@gmail.com>
2022-08-28 18:48:09 +1000

    Modified code

--- Assignment 1/as1_rscript.R
+++ Assignment 1/as1_rscript.R
@@ -63,33 +63,38 @@ beercons_ht1_mean = mean(beer_data2$beercons[beer_data2$hightax == 1])
 beercons_ht0_mean = mean(beer_data2$beercons[beer_data2$hightax == 0])
 
 beercons_ht1_sd = sd(beer_data2$beercons[beer_data2$hightax == 1])
+beercons_ht0_sd = sd(beer_data2$beercons[beer_data2$hightax == 0])
+
+beercons_ht1_se = beercons_ht1_sd/sqrt(length(beer_data2$beercons[beer_data2$hightax == 1]))
+beercons_ht0_se = beercons_ht0_sd/sqrt(length(beer_data2$beercons[beer_data2$hightax == 0]))
+
+mean_diff = beercons_ht1_mean - beercons_ht0_mean
+se_diff = sqrt((beercons_ht1_se**2 + beercons_ht0_se**2))
 
 # Conduct hypothesis testing:
-t_stat = (beercons_ht1_mean - beercons_ht0_mean)/beercons_ht1_sd
+t_stat = (mean_diff - 0)/se_diff
 p_value = 2 * pnorm(-abs(t_stat)) # p-value: 0.799
 
-# 95% CI for beercon when hightax = 1
-beercons_ht1_upper = beercons_ht1_mean + 1.96 * beercons_ht1_sd
-beercons_ht1_lower = beercons_ht1_mean - 1.96 * beercons_ht1_sd
+# 95% CI for mean diff
+mean_diff_upper = mean_diff + 1.96 * se_diff
+mean_diff_lower = mean_diff - 1.96 * se_diff
 
-# The 95% CI for beercon when hightax = 1 show that the mean of beercon when hightax = 0 lies within the interval
-# Hence, we failed to reject the null hypothesis
-# This might indicate that despite there is a high tax on beer, beer sales doesn't change by a lot
+diff_percentage = (abs(mean_diff)/beercons_ht0_mean) * 100 
 
-# Question 4
+# There is a change of approximately 4% when hightax went from 0 to 1.
 
+# Question 5
+
+#scatter plot of beertax against beercons
 beer_scatter = ggplot(beer_data2, aes(beertax, beercons)) + geom_point(colour = 'orange')
 beer_scatter + labs(title = "Scatter Plot for Beer Tax Against Beer Sales",
                   x = "Beer tax (dollars per gallon)",
-                  y = "Beer sales (gallons per capita)",
-                  fill = "Hightax value") +
+                  y = "Beer sales (gallons per capita)") +
                 theme_bw() +
                 theme(plot.title = element_text(face = "bold", size = 16, hjust = 0.5),
                       axis.title = element_text(face = "italic", size = 10)) +
                 geom_smooth(method = lm, se = FALSE, colour = "blue")
 
-beer_reg = lm(beercons~beertax, beer_data2)
-summary(beer_reg)
 
 cor_beertax_cons = cor(beer_data2$beertax, beer_data2$beercons)
 
@@ -97,20 +102,47 @@ cor_beertax_cons = cor(beer_data2$beertax, beer_data2$beercons)
 
 # Question 6
 
+# Single linear regression for beertax and beercons
+reg_1 = lm(beercons~beertax, beer_data2)
+summary(beer_reg)
+
 # Single linear regressions for cigtax and beercons, 
 reg_2 = lm(beercons~cigtax, beer_data2)
 summary(reg_2)
 
+# B_0 = 1.443 and B_1 = -0.0012
+# sd(B_0) = 0.0094745 and sd(B_1) = 0.0001919
+
+# Question 7
+
+# scatter plot for beercons and cigtax
+
+cons_cig_scatter = ggplot(beer_data2, aes(cigtax, beercons)) + geom_point(colour = 'orange')
+cons_cig_scatter + labs(title = "Scatter Plot for Cigarrette Tax Against Beer Sales",
+                        y = "Beer sales (dollars per gallon)",
+                        x = "Ciggarette tax (dollars per pack)") +
+                    theme_bw() +
+                    theme(plot.title = element_text(face = "bold", size = 16, hjust = 0.5),
+                          axis.title = element_text(face = "italic", size = 10)) +
+                    geom_smooth(method = lm, se = FALSE, colour = "blue")
 
 
+# scatter plot for beertax and cigtax
 
+beer_cig_scatter = ggplot(beer_data2, aes(beertax, cigtax)) + geom_point(colour = "orange")
+beer_cig_scatter + labs(title = "Scatter Plot for Beer Tax Against Cigarrette Tax",
+                        x = "Beer tax (dollars per gallon)",
+                        y = "Cigarrette tax (dollars per pack)") +
+                    theme_bw() +
+                    theme(plot.title = element_text(face = "bold", size = 16, hjust = 0.5),
+                          axis.title = element_text(face = "italic", size = 10)) +
+                    geom_smooth(method = lm, se = FALSE, colour = "blue")
 
+# QUestion 8
+reg_before_1994 = lm(beercons[year <= 1994]~beertax[year <= 1994], beer_data2)
+summary(reg_before_1994)
 
-
-
-
-
-
-
+reg_after_1994 = lm(beercons[year > 1994]~beertax[year > 1994], beer_data2)
+summary(reg_after_1994)
 
 
