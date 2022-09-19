37d1f1b6504406c4b5b558e48c6c617495b9569e
Skye Ha <nhatanhhado@gmail.com>
2022-09-19 13:10:06 +1000

    ecs dees

--- Assignment 2/as2_script.R
+++ Assignment 2/as2_script.R
@@ -13,7 +13,7 @@ dim(sleepData) # get number of observations and variables
 # Question 1: Summary statistic for the dataset.
 sleepStat = stargazer(sleepData,
                    summary.stat = c('n', 'mean', 'sd', 'median', 'min', 'max'),
-                   type = 'text', title = 'Descriptive Statistic for Beer Dataset',
+                   type = 'text', title = 'Descriptive Statistic for Sleep Dataset',
                    out = 'sleepStat.png')
 # Question 2: Constructing scatter plots
 
@@ -122,5 +122,98 @@ pValue5C
 # much greater than 0.05. Hence, we failed to reject null hypothesis. It is statiscally significantly indifferent from 0.
 
 #5D
+
+#5E
+# using waldtest()
+waldtest(reg5, vcov = vcovHC(reg5, 'HC1'))
+
+# F-stat: 2.3055 ; df = 9; p-val = 0.01482 ( < 0.05)
+# 
+
+# Question 6:
+q6Reg1 = reg5
+q6Cov1 = cov5
+q6se1 = se5
+
+q6Reg2 = lm(nap~educ + age + gdhlth + smsa + union + selfe + marr + yrsmarr + yngkid, sleepData)
+q6Cov2 = vcovHC(q6Reg2, type = "HC1")
+q6se2 = sqrt(diag(cov5))
+
+# Build the regression table
+stargazer(q6Reg1, q6Reg2, type="text",
+          se=list(q6se1, q6se2),
+          digits=2, 
+          dep.var.labels=c("Number of minutes sleep per week", "Number of minutes napping per week"),
+          covariate.labels=
+            c("Number of years of education attainment",
+              "Age",
+              "Self-reported Health",
+              "Live in US urban area",
+              "Part of a union",
+              "Self-employed",
+              "Marrital status",
+              "Number of years Married",
+              "Have children that is less than 3 years old"),
+          out="q6_output.txt")
+
+coefDifq6 = -9.95 - (-4.85)
+seDifq6 = sqrt(5.84**2 + 5.84**2)
+tStatq6 = (coefDifq6 - 0)/seDifq6
+pValueq6 = 2 * pnorm(-abs(tStatq6))
+pValueq6
+
+# p-value > 0.1; hence we failed to reject the null hypothesis. Their difference is statistically significantly
+# indifferent from 0.
+
+# Question 8:
+
+q7Reg1 = lm(sleep ~ totwrk + educ + age + gdhlth + smsa + union + selfe + marr + yrsmarr + yngkid, sleepData)
+q7cov1 = vcovHC(q7Reg1, type = 'HC1')
+q7se1 = sqrt(diag(q7cov1))
+
+q7Reg2 = lm(sleep ~ totwrk + educ + age + gdhlth + smsa + union + selfe + marr + yrsmarr + yngkid, sleepData[sleepData$male == 1,])
+q7cov2 = vcovHC(q7Reg2, type = 'HC1')
+q7se2 = sqrt(diag(q7cov2))
+
+q7Reg3 = lm(sleep ~ totwrk + educ + age + gdhlth + smsa + union + selfe + marr + yrsmarr + yngkid, sleepData[sleepData$male == 0,])
+q7cov3 = vcovHC(q7Reg3, type = 'HC1')
+q7se3 = sqrt(diag(q7cov3))
+
+stargazer(q7Reg1, q7Reg2, q7Reg3, type = 'text',
+          se = list(q7se1, q7se2, q7se3),
+          digits = 2,
+          dep.var.labels=c("Number of minutes sleep per week"),
+          column.labels = c("Full Dataset", "Male", "Female"),
+          covariate.labels=
+            c("Number of minutes working per week", 
+              "Number of years of education attainment",
+              "Age",
+              "Self reported health is \"Excellent\" or \"Good\"",
+              "Lives in a US urban area",
+              "Part of a union",
+              "Self-employed",
+              "Marrital status",
+              "Number of years married",
+              "Have a child less than 3 at home"),
+          out="q7_output.txt")
+
+# Question 8
+
+# Conducting t-test for Reg1 on totwrk
+tStatq8 = (-0.13 - 0)/0.02
+pValueq8 = 2 * pnorm(-abs(tStatq8))
+pValueq8
+
+# p-value less than 0.05, reject null hypothesis. It is statistically significantly different from 0.
+# a increase in 5 hours = 300 minutes of working  results in a decrease of 39 minutes for sleeping.
+
+#8B
+linearHypothesis(q7Reg1, c("marr = yngkid"), vcov = vcovHC(q7Reg1, "HC1"))
+
+#F-stat = 1.1792. p-value = 0.2779. AT 5% significance level, we fail to reject the null hypothesis.
+# The coefficient for marr and yngkid are statistically significantly indifferent
+
+#8C
+
 
 
